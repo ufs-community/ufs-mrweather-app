428afc81c0a9a84b4695961a957190f23d1d42ef
Ufuk Turuncoglu <turuncu@ucar.edu>
2020-01-24 12:52:06 -0700

    sphinx_rtd_theme is added to extension list

--- docs/UsersGuide/source/conf.py
+++ docs/UsersGuide/source/conf.py
@@ -50,7 +50,8 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
