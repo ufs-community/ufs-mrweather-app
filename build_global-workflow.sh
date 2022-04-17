a2515bfe3be94776c9c936ebfa9a036b72651dde
JONG KIM <jong.kim@noaa.gov>
2022-04-16 21:24:39 -0400

    A bug fix on build log check

--- build_global-workflow.sh
+++ build_global-workflow.sh
@@ -29,12 +29,12 @@ sed -i '6s/yes/no/g' fv3gfs_build.cfg
 if [ $UFS_BUILD_OPTION == "-c" ]
 then
     sh build_all.sh -c
-    [[ -f log/build_ufs.log ]] && cd $CUR_PWD; echo "Error: log/build_ufs.log doesn't exist." >&2; exit 1
+    [[ ! -f log/build_ufs.log ]] && cd $CUR_PWD; echo "Error: log/build_ufs.log does not exist." >&2; exit 1
     target=$(grep 'target=' build_ufs.log | awk -F. '{ print $1 }' | awk -F= '{print $2}' )
     sh link_workflow.sh emc $target coupled
 else
     sh build_all.sh
-    [[ -f log/build_ufs.log ]] && cd $CUR_PWD; echo "Error: log/build_ufs.log doesn't exist." >&2; exit 1
+    [[ ! -f log/build_ufs.log ]] && cd $CUR_PWD; echo "Error: log/build_ufs.log does not exist." >&2; exit 1
     target=$(grep 'target=' build_ufs.log | awk -F. '{ print $1 }' | awk -F= '{print $2}' )
     sh link_workflow.sh emc $target
 fi
