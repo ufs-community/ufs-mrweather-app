5d0d43888cfe20745d76a32fdb5e02479fdf5971
JONG KIM <jong.kim@noaa.gov>
2022-04-16 22:14:49 -0400

    Cleaned up build script

--- build_global-workflow.sh
+++ build_global-workflow.sh
@@ -14,29 +14,31 @@ do
 		break
 		;;	    
         esac
-
 done
 
 echo "current path" $(pwd);
-CUR_PWD=$(pwd);
+CUR_PWD=$(pwd)
+cd global-workflow/sorc; WRK_PWD=$(pwd)
+sh checkout.sh                                                                                                                               
 
-cd global-workflow/sorc
-sh checkout.sh
+# turn off gsi build option                                                                                                                   
+sed -i '6s/yes/no/g' fv3gfs_build.cfg                                                                                                        
 
-# turn off gsi build option
-sed -i '6s/yes/no/g' fv3gfs_build.cfg
-
-if [ $UFS_BUILD_OPTION == "-c" ]
-then
-    sh build_all.sh -c
-    [[ ! -f log/build_ufs.log ]] && cd $CUR_PWD; echo "Error: log/build_ufs.log does not exist." >&2; exit 1
-    target=$(grep 'target=' build_ufs.log | awk -F. '{ print $1 }' | awk -F= '{print $2}' )
-    sh link_workflow.sh emc $target coupled
+# build and link components -------------------------------------------------------                                                           
+if [ $UFS_BUILD_OPTION == "-c" ]; then
+  sh build_all.sh -c                                                                                                                         
+  logfile="logs/build_ufs.log"
+  if [[ -f $logfile ]] ; then
+    target=$(grep 'target=' $logfile | awk -F. '{print $1}' | awk -F= '{print $2}')
+    sh link_workflow.sh emc $target coupled; cd $CUR_PWD; exit 0
+  fi
 else
-    sh build_all.sh
-    [[ ! -f log/build_ufs.log ]] && cd $CUR_PWD; echo "Error: log/build_ufs.log does not exist." >&2; exit 1
-    target=$(grep 'target=' build_ufs.log | awk -F. '{ print $1 }' | awk -F= '{print $2}' )
-    sh link_workflow.sh emc $target
+  sh build_all.sh                                                                                                                            
+  logfile="logs/build_ufs.log"
+  if [[ -f $logfile ]] ; then
+    target=$(grep 'target=' $logfile | awk -F. '{print $1}' | awk -F= '{print $2}')
+    sh link_workflow.sh emc $target; cd $CUR_PWD; exit 0
+  fi
 fi
 
-cd $CUR_PWD
+[[ -f "$WRK_PWD/logs/build_ufs.log" ]] && cd $CUR_PWD; echo "Error: logs/build_ufs.log does not exist." >&2; exit 1
